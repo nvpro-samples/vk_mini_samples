3846ce2a7606737ef92cb07558c055a00ad51219
Martin-Karl Lefrançois <mlefrancois@nvidia.com>
2024-01-15 12:00:05 +0100

    New sample: GPU Monitor

---
+++ samples/gpu_monitor/README.md
@@ -0,0 +1,5 @@
+# GPU Monitor
+
+![](gpu_monitor.png)
+
+This example doesn't show any Vulkan specifics, but shows how to add the NVML element to the framework to display GPU usage. This is done by using the [NVIDIA Management Library (NVML)](https://developer.nvidia.com/nvidia-management-library-nvml) and retrieving the information at certain intervals. This data is displayed in a window that can be docked anywhere. 
---
+++ samples/gpu_monitor/gpu_monitor.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+/*
+  This sample is not using any shaders, it is simply showing the GPU usage
+  
+*/
+
+
+#include <array>
+#include <vulkan/vulkan_core.h>
+
+#include "nvvkhl/application.hpp"
+#include "nvvkhl/element_nvml.hpp"
+
+int main(int argc, char** argv)
+{
+  nvvkhl::ApplicationCreateInfo spec;
+  spec.name                  = std::string(PROJECT_NAME) + " Example";
+  spec.vSync                 = true;
+  spec.vkSetup.apiMajor      = 1;
+  spec.vkSetup.apiMinor      = 3;
+  spec.hasUndockableViewport = false;
+  spec.width                 = 750;
+  spec.height                = 400;
+
+
+  // Setting up the layout of the application. Docking the NVML monitor in the center
+  spec.dockSetup = [](ImGuiID viewportID) { ImGui::DockBuilderDockWindow("NVML Monitor", viewportID); };
+
+  // Create the application
+  auto app = std::make_unique<nvvkhl::Application>(spec);
+
+  // The NVML (GPU) monitor
+  app->addElement(std::make_shared<nvvkhl::ElementNvml>(true));
+  app->run();
+
+  return 0;
+}
